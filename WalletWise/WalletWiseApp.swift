1bfe7dabc3129af2c9be962257acc96e7f3e3d32
Nicolas Zwierzykowski <nicolaszwier@gmail.com>
2024-04-29 00:44:25 -0400

    Initial Commit

---
+++ WalletWise/WalletWiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalletWiseApp.swift
+//  WalletWise
+//
+//  Created by NicolasZwierzykowski on 29/04/24.
+//
+
+import SwiftUI
+
+@main
+struct WalletWiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
