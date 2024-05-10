24fb216dfea41db96071d53b201b097f7d282b5d
Nicolas Zwierzykowski <nicolaszwier@gmail.com>
2024-05-09 20:51:29 -0400

    new: Button ui component

--- WalletWise/UI/Button/WWButton.swift
+++ WalletWise/UI/Button/WWButton.swift
@@ -8,11 +8,24 @@
 import SwiftUI
 
 struct WWButton: View {
+    let label: String
+    let background: Color
+    let action: () -> Void
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button(label) {
+           action()
+        }
+        .frame(height: 37.0)
+        .fontWeight(.heavy)
+        .frame(maxWidth: /*@START_MENU_TOKEN@*/.greatestFiniteMagnitude/*@END_MENU_TOKEN@*/)
+        .background(background)
+        .foregroundColor(.white)
+        .clipShape(RoundedRectangle(cornerRadius: 8, style: /*@START_MENU_TOKEN@*/.continuous/*@END_MENU_TOKEN@*/))
     }
 }
 
 #Preview {
-    WWButton()
+    WWButton(label: "Label", background: .blue, action: {
+        
+    })
 }
