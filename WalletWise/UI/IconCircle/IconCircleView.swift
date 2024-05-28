075dcabbef6abb2cc39167fec132024ca2a1805f
Nicolas Zwierzykowski <nicolaszwier@gmail.com>
2024-05-28 17:36:39 -0400

    new: colored icon circle view

--- WalletWise/UI/IconCircle/IconCircleView.swift
+++ WalletWise/UI/IconCircle/IconCircleView.swift
@@ -8,11 +8,31 @@
 import SwiftUI
 
 struct IconCircleView: View {
+    let icon: String
+
+    let circleColor: Color
+    let imageColor: Color // Remove this for an image in your assets folder.
+    let frameSize: CGFloat
+    var squareSide: CGFloat {
+        1.2.squareRoot() * frameSize
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Circle()
+                .fill(circleColor)
+                .frame(width: frameSize * 2, height: frameSize * 2)
+            
+            Image(systemName: icon)
+                .resizable()
+                .aspectRatio(1.0, contentMode: .fit)
+                .frame(width: squareSide, height: squareSide)
+                .foregroundColor(imageColor)
+        }
+        
     }
 }
 
 #Preview {
-    IconCircleView()
+    IconCircleView(icon: "cart.fill", circleColor: .blue, imageColor: .white, frameSize: 25)
 }
