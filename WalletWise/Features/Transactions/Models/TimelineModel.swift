eec165e540fcdde7c7954ea94a178a216bab9f31
Nicolas Zwierzykowski <nicolaszwier@gmail.com>
2024-05-28 17:43:12 -0400

    update: added filters to timeline model

--- WalletWise/Features/Transactions/Models/TimelineModel.swift
+++ WalletWise/Features/Transactions/Models/TimelineModel.swift
@@ -9,9 +9,9 @@ import Foundation
 
 class TimelineModel {
     
-    func fetch(planningId: String) async throws -> [Period] {
-        
-        guard let request = try? HttpService().buildUrlRequest(method: "GET", endpoint: Constants.ApiConstants.Periods.getPeriods, params: [planningId]) else {
+    func fetch(planningId: String, filters: FiltersEntity) async throws -> [Period] {
+        let queryParams = self.transformFiltersToQueryParams(filters: filters)
+        guard let request = try? HttpService().buildUrlRequest(method: "GET", endpoint: Constants.ApiConstants.Periods.getPeriods, params: [planningId], queryParams: queryParams) else {
             throw NetworkError.invalidURL
         }
         
@@ -38,5 +38,25 @@ class TimelineModel {
         }
   
       }
+    
+    private func transformFiltersToQueryParams(filters: FiltersEntity) -> [URLQueryItem] {
+        var result: [URLQueryItem] = []
+        if filters.selectedPeriod.rawValue == FilterPeriod.lastMonth.rawValue {
+            result.append(URLQueryItem(name: "startDate", value: UtilsService().addOrSubtractMonth(month: -1).ISO8601Format()))
+            result.append(URLQueryItem(name: "endDate", value: UtilsService().addOrSubtractDay(day: 7).ISO8601Format()))
+        }
+        if filters.selectedPeriod.rawValue == FilterPeriod.lastThreeMonths.rawValue {
+            result.append(URLQueryItem(name: "startDate", value: UtilsService().addOrSubtractMonth(month: -3).ISO8601Format()))
+            result.append(URLQueryItem(name: "endDate", value: UtilsService().addOrSubtractDay(day: 7).ISO8601Format()))
+        }
+        if filters.selectedPeriod.rawValue == FilterPeriod.custom.rawValue {
+            result.append(URLQueryItem(name: "startDate", value: filters.startDate.ISO8601Format()))
+            result.append(URLQueryItem(name: "endDate", value: filters.endDate.ISO8601Format()))
+        }
+        result.append(URLQueryItem(name: "includeExpenses", value: filters.includeExpenses.description))
+        result.append(URLQueryItem(name: "includeIncomes", value: filters.includeIncomes.description))
+        
+        return result
+    }
     
 }
