2758b27caefae73281ad331211db09fa729cde02
Nicolas Zwierzykowski <nicolaszwier@gmail.com>
2024-06-03 22:40:53 -0400

    feat: added new Color

---
+++ WalletWise/UI/Styles/OutlinedTextFieldStyle.swift
@@ -0,0 +1,18 @@
+//
+//  OutlinedTextFieldStyle.swift
+//  WalletWise
+//
+//  Created by NicolasZwierzykowski on 03/06/24.
+//
+
+import SwiftUI
+
+struct OutlinedTextFieldStyle: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OutlinedTextFieldStyle()
+}
---
+++ WalletWise/UI/Styles/UnderlinedTextFieldStyle.swift
@@ -0,0 +1,8 @@
+//
+//  UnderlinedTextFieldStyle.swift
+//  WalletWise
+//
+//  Created by NicolasZwierzykowski on 03/06/24.
+//
+
+import Foundation
