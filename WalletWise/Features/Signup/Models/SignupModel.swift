34f3848fbbee1f42adc45d2ff9b78fdb0a9d285e
Nicolas Zwierzykowski <nicolaszwier@gmail.com>
2024-07-10 19:32:55 -0400

    feat: added Signup view

--- WalletWise/Features/Signup/Models/SignupModel.swift
+++ WalletWise/Features/Signup/Models/SignupModel.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+
+
+class SignupModel {
+    
+    func signup(data: SignupRequest) async throws -> SignupResponse {
+        
+        guard var request = try? HttpService().buildUrlRequest(method: "POST", endpoint: Constants.ApiConstants.Auth.signup, params: []) else {
+            throw NetworkError.invalidURL
+        }
+        
+        request.httpBody = try? HttpService().customEncoder().encode(data)
+        
+        let (data, response) = try await URLSession.shared.data(for: request)
+        guard response is HTTPURLResponse else {
+            throw NetworkError.custom(errorMessage: "Invalid HTTPUrlResponse")
+        }
+        print(String(data: data, encoding: .utf8)!)
+        let decodedResponse = try? HttpService().customDecoder().decode(SignupResponse.self, from: data)
+        
+        if (decodedResponse?.accessToken != nil) {
+            return decodedResponse!
+        }
+        
+        try HttpService().handleError(data: data, statusCode: decodedResponse?.statusCode ?? nil)
+        
+        throw NetworkError.noData
+    }
+      
+}
