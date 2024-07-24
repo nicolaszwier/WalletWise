599be5a16a48f509582535a0ca933febf1ce4a4a
Nicolas Zwierzykowski <nicolaszwier@gmail.com>
2024-07-23 22:20:19 -0400

    new: icon rounded rectangle

--- WalletWise/UI/IconRoundedRectangle/IconRoundedRectangleView.swift
+++ WalletWise/UI/IconRoundedRectangle/IconRoundedRectangleView.swift
@@ -8,11 +8,31 @@
 import SwiftUI
 
 struct IconRoundedRectangleView: View {
+    let icon: String
+
+    let circleColor: Color
+    let imageColor: Color // Remove this for an image in your assets folder.
+    let frameSize: CGFloat
+    var squareSide: CGFloat {
+        1.2.squareRoot() * frameSize
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            RoundedRectangle(cornerRadius: 12)
+                .fill(circleColor)
+                .frame(width: frameSize * 2, height: frameSize * 2)
+            
+            Image(systemName: icon)
+                .resizable()
+                .aspectRatio(1.0, contentMode: .fit)
+                .frame(width: squareSide, height: squareSide)
+                .foregroundColor(imageColor)
+        }
+        
     }
 }
 
 #Preview {
-    IconRoundedRectangleView()
+    IconRoundedRectangleView(icon: "cart.fill", circleColor: .blue, imageColor: .white, frameSize: 25)
 }
